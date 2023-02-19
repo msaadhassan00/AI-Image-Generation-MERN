1576d4ac7c425839c014f7ed75f658400eedfc52
msaadhassan00 <msaadhassan00@gmail.com>
2023-02-20 00:05:19 +0500

    commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
     const fetchPost = async () => {
       setLoading(true);
       try {
-        const response = await fetch('http://localhost:5000/api/v1/post', {
+        const response = await fetch('https://api-dall-e.netlify.app//api/v1/post', {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
